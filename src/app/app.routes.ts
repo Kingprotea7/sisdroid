45680eddc5b9cf1e78c5d654e1802acb525abbdc
Fabian Alfaro <Ranshesi7@gmail.com>
2024-10-01 09:37:49 -0500

    segunda version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'menu',
+    loadComponent: () => import('./menu/menu.page').then( m => m.MenuPage)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'menu',
     pathMatch: 'full',
-  },
+  }
 ];
